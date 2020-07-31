79c2337cc9e5dea78ffe43fc83d6cb3f3f92c725
Shankhadeep1234 <shankhadeepraj@gmail.com>
2020-07-31 20:18:28 +0530

    done with firebase setup and upload form

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import * as firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyC6wpCMP1mrPRlhlo5XND5jzU4PFIEK-FI",
+  authDomain: "firegram-ab28a.firebaseapp.com",
+  databaseURL: "https://firegram-ab28a.firebaseio.com",
+  projectId: "firegram-ab28a",
+  storageBucket: "firegram-ab28a.appspot.com",
+  messagingSenderId: "129735229665",
+  appId: "1:129735229665:web:46af8da85d845d2245b7a0",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFireStore = firebase.firestore();
+
+export { projectStorage, projectFireStore };
